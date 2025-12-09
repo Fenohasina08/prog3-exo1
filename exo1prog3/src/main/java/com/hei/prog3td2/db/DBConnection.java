9c06467ef6b9a9c11a80e9180e8ed2dd20523b69
fenohasina <fenohasinaherimamy@gmail.com>
2025-12-09 16:37:05 +0300

    feat: add DBConnection class

---
+++ exo1prog3/src/main/java/com/hei/prog3td2/db/DBConnection.java
@@ -0,0 +1,15 @@
+package com.hei.prog3td2.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    private static final String JDBC_URL = "jdbc:postgresql://localhost:5432/product_management_db";
+    private static final String USER = "rafano";
+    private static final String PASSWORD = "123456";
+
+    public static Connection getDBConnection() throws SQLException {
+        return DriverManager.getConnection(JDBC_URL, USER, PASSWORD);
+    }
+}
