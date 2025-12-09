986b4f6d6b3a5b8a37c9b53503045cb2ffe9a2ad
HerimamyRa-fanomezana <fenohasina.ra_fanomezana@student.passerellesnumerIques.org>
2025-12-09 17:11:54 +0300

    Delete prog3jdbc directory

--- prog3jdbc/src/main/java/com/linkdatabase/prog3jdbc/HomeController.java
+++
@@ -1,9 +0,0 @@
-package com.linkdatabase.prog3jdbc;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-public class HomeController {
-    @GetMapping("/")
-    public String home() {
-        return "API Spring Boot opérationnelle !";
-    }
-}
